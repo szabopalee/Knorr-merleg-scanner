5b7c0a6c2bf95a5d915dbcd256c9383806454bea
szabopalee <72488741+szabopalee@users.noreply.github.com>
2020-10-28 11:23:18 +0100

    Add files via upload

--- merleg.py
+++ merleg.py
@@ -1,5 +1,19 @@
-import os
+#import os
+#cmd = "echo S | netcat 172.27.51.35 4305"
+#os.system(cmd)
 
-cmd = "echo S | netcat 172.27.51.35 4305"
 
-os.system(cmd)
+import socket
+
+TCP_IP = '172.27.51.35' # this IP of my pc. When I want raspberry pi 2`s as a client, I replace it with its IP '169.254.54.195'
+TCP_PORT = 4035
+BUFFER_SIZE = 1024
+MESSAGE = "S"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(MESSAGE)
+data = s.recv(BUFFER_SIZE)
+s.close()
+
+print ("received data:", data)
